e79df2119ec6e1d44882aa7fce198307008070aa
Wandole <mchirez@gmail.com>
2022-09-14 12:43:20 +0200

    Add notation logic and style

--- src/components/Notation/Notation.js
+++ src/components/Notation/Notation.js
@@ -1,5 +1,20 @@
+import './notation.css'
+import star from '../../assets/star.svg'
+import fullStar from '../../assets/full-star.svg'
+
 function Notation(props) {
-  return <>Notation</>
+  const { rating } = props
+
+  const stars = []
+  for (let i = 0; i < 5; i++) {
+    if (i < rating) {
+      stars.push(<img src={fullStar} alt="" />)
+    } else {
+      stars.push(<img src={star} alt="" />)
+    }
+  }
+
+  return <section className="notation">{stars}</section>
 }
 
 export default Notation
