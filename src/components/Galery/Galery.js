c2661290c70974597dc1a6278e70d625a7ec16a5
Wandole <mchirez@gmail.com>
2022-09-14 11:55:22 +0200

    Add galery style

--- src/components/Galery/Galery.js
+++ src/components/Galery/Galery.js
@@ -1,16 +1,16 @@
 import { useState, useEffect } from 'react'
-
+import prev from '../../assets/prev.svg'
+import next from '../../assets/next.svg'
+import './galery.css'
 function Galery(props) {
   const pictures = props.pictures
   const [currImg, setCurrImg] = useState(0)
   const [images, setImages] = useState()
   useEffect(() => {
     setImages(
-      <div className="images">
-        <img src={pictures.at(currImg - 1)} alt="" />
-        <img src={pictures.at(currImg)} alt="" />
-        <img src={pictures.at((currImg + 1) % pictures.length)} alt="" />
-      </div>
+      <>
+        <img className="galery-img" src={pictures.at(currImg)} alt="" />
+      </>
     )
   }, [currImg])
 
@@ -18,21 +18,23 @@ function Galery(props) {
     let newImg = currImg - 1
     if (newImg < 0) newImg = pictures.length - 1
     setCurrImg(newImg)
-    console.log(newImg)
   }
 
   const handleNextImg = () => {
     let newImg = currImg + 1
     if (newImg >= pictures.length) newImg = 0
     setCurrImg(newImg)
-    console.log(newImg)
   }
 
   return (
     <section className="galery">
-      <button onClick={handlePrevImg}>Prev</button>
+      <button className="prev-btn" onClick={handlePrevImg}>
+        <img className="btn-img" src={prev} alt="" />
+      </button>
       {images}
-      <button onClick={handleNextImg}>Next</button>
+      <button className="next-btn" onClick={handleNextImg}>
+        <img className="btn-img" src={next} alt="" />
+      </button>
     </section>
   )
 }
