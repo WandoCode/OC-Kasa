f9f657628617d15c2762d89c061159ad6c7ba3e8
Wandole <mchirez@gmail.com>
2022-09-14 11:00:45 +0200

    Add router logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
+import Location from './pages/Location/Location'
+import PageNotFound from './pages/PageNotFound/PageNotFound'
 import store from './store/location'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import About from './pages/About/About'
+import Layout from './components/Layout/Layout'
 
 function App() {
+  // TODO: passer à Home
   const [locationsDatas, setLocationsDatas] = useState()
 
   useEffect(() => {
@@ -15,9 +21,20 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <Home locationsDatas={locationsDatas} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            path="home"
+            element={<Home locationsDatas={locationsDatas} />}
+          />
+          <Route path="about" element={<About />} />
+          <Route path="/location/:id" element={<Location />} />
+          <Route path="not-found" element={<PageNotFound />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
